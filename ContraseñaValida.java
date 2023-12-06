270905353e4bc1db54887e38c47d80a2c2293370
Karen <karen.lopez.saez@gmail.com>
2023-12-06 21:29:30 +0100

    Modificacions des del repositori local

--- ContraseñaValida.java
+++ ContraseñaValida.java
@@ -2,73 +2,76 @@ package RandomStringDate;
 
 import java.util.Scanner;
 
+
 public class ContraseñaValida {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		
-		        Scanner sc = new Scanner(System.in);
+        boolean contrasenaValida = false;
 
-		        boolean contrasenaValida = false;
+        while (!contrasenaValida) {
+            System.out.print("Introduce una contraseña: ");
+            String contrasena = sc.nextLine();
 
-		        while (!contrasenaValida) {
-		            System.out.print("Introduce una contraseña: ");
-		            String contrasena = sc.nextLine();
+            // Validar la contrasenya utilitzant la nova funció
+            contrasenaValida = validarContrasena(contrasena);
 
-		            // Validar la contraseña
-		            int longitud = contrasena.length();
-		            int mayusculas = 0;
-		            int minusculas = 0;
-		            int digitos = 0;
-		            int caracteresEspeciales = 0;
+            if (!contrasenaValida) {
+                System.out.println("Contraseña no válida. Inténtalo de nuevo.");
+            }
+        }
 
-		            // Contar letras mayúsculas, minúsculas, dígitos y caracteres especiales
-		            for (char c : contrasena.toCharArray()) {
-		                if (Character.isUpperCase(c)) {
-		                    mayusculas++;
-		                } else if (Character.isLowerCase(c)) {
-		                    minusculas++;
-		                } else if (Character.isDigit(c)) {
-		                    digitos++;
-		                } else if ("!\"$%&/()".indexOf(c) != -1) {
-		                    caracteresEspeciales++;
-		                }
-		            }
+        System.out.println("¡Contraseña válida!");
 
-		            // Mostrar errores si la contraseña no es válida
-		            if (longitud < 8) {
-		                System.out.println("- La contraseña debe tener al menos 8 caracteres.");
-		            }
+        sc.close();
+    }
 
-		            if (mayusculas < 2) {
-		                System.out.println("- La contraseña debe tener al menos 2 letras mayúsculas. Actualmente tienes " + mayusculas + ".");
-		            }
+    // Nova funció per validar la contrasenya
+    private static boolean validarContrasena(String contrasena) {
+        int longitud = contrasena.length();
+        int mayusculas = 0;
+        int minusculas = 0;
+        int digitos = 0;
+        int caracteresEspeciales = 0;
 
-		            if (minusculas < 3) {
-		                System.out.println("- La contraseña debe tener al menos 3 letras minúsculas. Actualmente tienes " + minusculas + ".");
-		            }
+        for (char c : contrasena.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                mayusculas++;
+            } else if (Character.isLowerCase(c)) {
+                minusculas++;
+            } else if (Character.isDigit(c)) {
+                digitos++;
+            } else if ("!\"$%&/()".indexOf(c) != -1) {
+                caracteresEspeciales++;
+            }
+        }
 
-		            if (digitos == 0) {
-		                System.out.println("- La contraseña debe tener al menos un dígito.");
-		            }
+        if (longitud < 8) {
+            System.out.println("- La contraseña debe tener al menos 8 caracteres.");
+        }
 
-		            if (caracteresEspeciales == 0) {
-		                System.out.println("- La contraseña debe tener al menos uno de estos caracteres: !\"$%&/()");
-		            }
+        if (mayusculas < 2) {
+            System.out.println("- La contraseña debe tener al menos 2 letras mayúsculas. Actualmente tienes " + mayusculas + ".");
+        }
 
-		            if (contrasena.contains("@")) {
-		                System.out.println("- La contraseña no debe contener el carácter @.");
-		            }
+        if (minusculas < 3) {
+            System.out.println("- La contraseña debe tener al menos 3 letras minúsculas. Actualmente tienes " + minusculas + ".");
+        }
 
-		            // Verificar si la contraseña es válida
-		            if (longitud >= 8 && mayusculas >= 2 && minusculas >= 3 && digitos > 0 && caracteresEspeciales > 0 && !contrasena.contains("@")) {
-		                System.out.println("¡Contraseña válida!");
-		                contrasenaValida = true;
-		            } else {
-		                System.out.println("Contraseña no válida. Inténtalo de nuevo.");
-		            }
-		        }
+        if (digitos == 0) {
+            System.out.println("- La contraseña debe tener al menos un dígito.");
+        }
 
-		        sc.close();
-		    }
-		}
+        if (caracteresEspeciales == 0) {
+            System.out.println("- La contraseña debe tener al menos uno de estos caracteres: !\"$%&/()");
+        }
+
+        if (contrasena.contains("@")) {
+            System.out.println("- La contraseña no debe contener el carácter @.");
+        }
+
+        return (longitud >= 8 && mayusculas >= 2 && minusculas >= 3 && digitos > 0 &&
+                caracteresEspeciales > 0 && !contrasena.contains("@"));
+    }
+}
